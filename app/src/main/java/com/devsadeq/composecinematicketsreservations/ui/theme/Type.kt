2f77164f0bd618510c402dab354fdd2a05c52737
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-07-03 16:48:13 +0300

    create typography

--- app/src/main/java/com/devsadeq/composecinematicketsreservations/ui/theme/Type.kt
+++ app/src/main/java/com/devsadeq/composecinematicketsreservations/ui/theme/Type.kt
@@ -2,33 +2,65 @@ package com.devsadeq.composecinematicketsreservations.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.devsadeq.composecinematicketsreservations.R
 
-// Set of Material typography styles to start with
+val OpenSans = FontFamily(
+    Font(R.font.opensans_regular),
+    Font(R.font.opensans_medium, FontWeight.W500),
+)
+
 val Typography = Typography(
+    headlineMedium = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
 )
